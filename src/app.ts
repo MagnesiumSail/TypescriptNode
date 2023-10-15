4dc60506be7435a198b45e319b6ffa0f2699c62f
MagnesiumSail <Leapster16@gmail.com>
2023-10-15 09:17:42 -0600

    Added Typescript and Node

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// Variable declaration with type annotation
+let message: string = "Hello, TypeScript!";
+console.log(message);
+
+// Type inference (TypeScript can infer types)
+let count = 5;
+console.log(count);
