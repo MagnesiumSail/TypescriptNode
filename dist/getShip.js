05ee66a1c4c349e6469fa22e6b4064a61684b18b
MagnesiumSail <Leapster16@gmail.com>
2023-10-16 19:21:46 -0600

    Modularized getShip better

--- dist/getShip.js
+++ dist/getShip.js
@@ -4,13 +4,19 @@ const utils_1 = require("./utils");
 let base = 'https://api.spacetraders.io/v2';
 let responseJSON = {};
 const Looker = (0, utils_1.createHeaders)();
-fetch(`${base}/my/agent`, { headers: Looker })
-    .then(response => response.json())
-    .then(response => {
-    //console.log(response)
-    const hq = response.data.headquarters;
-    const acceptURL = `${base}/systems/${hq.slice(0, hq.lastIndexOf('-'))}/waypoints`;
-    //console.log(acceptURL)
+function getLocation() {
+    fetch(`${base}/my/agent`, { headers: Looker })
+        .then(response => response.json())
+        .then(response => {
+        //console.log(response)
+        const hq = response.data.headquarters;
+        const acceptURL = `${base}/systems/${hq.slice(0, hq.lastIndexOf('-'))}/waypoints`;
+        findShipyard(acceptURL);
+    })
+        .catch(err => console.error(err));
+}
+//console.log(acceptURL)
+function findShipyard(acceptURL) {
     fetch(acceptURL, { headers: Looker })
         .then(response => response.json())
         .then(response => {
@@ -27,6 +33,6 @@ fetch(`${base}/my/agent`, { headers: Looker })
         console.log(shipyards);
     })
         .catch(err => console.error(err));
-})
-    .catch(err => console.error(err));
+}
 let shipyards = [];
+getLocation();
--- src/getShip.ts
+++ src/getShip.ts
@@ -4,33 +4,42 @@ let base: string = 'https://api.spacetraders.io/v2';
 let responseJSON: any = {};
 const Looker: HeadersType = createHeaders();
 
-fetch(`${base}/my/agent`, { headers: Looker })
-    .then(response => response.json())
-    .then(response => {
-        //console.log(response)
-        const hq = response.data.headquarters;
-        const acceptURL = `${base}/systems/${hq.slice(0, hq.lastIndexOf('-'))}/waypoints`;
-        //console.log(acceptURL)
-        fetch(acceptURL, { headers: Looker })
-            .then(response => response.json())
-            .then(response => { 
-                responseJSON = response;
-                responseJSON.data.forEach((object: DataObject) => {
-                    if (object.traits) {
-                        object.traits.forEach((trait: Trait) => {
-                            if (trait.name === "Shipyard") {
-                                shipyards.push(object);
-                            }
-                        })
-                    }
-                })
-                console.log(shipyards);
-                
+function getLocation() {
+    fetch(`${base}/my/agent`, { headers: Looker })
+        .then(response => response.json())
+        .then(response => {
+            //console.log(response)
+            const hq = response.data.headquarters;
+            const acceptURL: string = `${base}/systems/${hq.slice(0, hq.lastIndexOf('-'))}/waypoints`;
+            findShipyard(acceptURL);
+            
+        })
+        .catch (err => console.error(err));
+}
+//console.log(acceptURL)
+function findShipyard(acceptURL: string) {
+    fetch(acceptURL, { headers: Looker })
+        .then(response => response.json())
+        .then(response => {
+            responseJSON = response;
+            responseJSON.data.forEach((object: DataObject) => {
+                if (object.traits) {
+                    object.traits.forEach((trait: Trait) => {
+                        if (trait.name === "Shipyard") {
+                            shipyards.push(object);
+                        }
+                    })
+                }
             })
-            .catch(err => console.error(err));
-    })
-    .catch(err => console.error(err));
+            console.log(shipyards);
+
+        })
+        .catch (err => console.error(err));
+}
+    
 
 let shipyards: DataObject[] = [];
+
+getLocation();
 
 
